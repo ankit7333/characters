f77c65575079911ee4f9366b4ae60624e154a3d2
ankitadulkar <ankit.adulkar1@justdial.com>
2024-01-26 16:07:54 +0530

    as data flow from parent to child so i removed hooks from child component to parent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import './styles/global.scss';
 import ListView from './components/ListView'
 function App() {
+  const [results, setResults] = useState([]);
+    const [isloading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    useEffect(function(){
+        async function getCharaterDetails(){
+            try{
+                setLoading(true) 
+                setError('')
+                const res = await fetch(`https://rickandmortyapi.com/api/character`);
+                const data = await res.json();
+                setResults(data.results)
+                setLoading(false) 
+                console.log(data.results)
+            }catch(err){
+                console.error(`Error`, error)
+            }
+        }
+        getCharaterDetails()
+    },[])
   return (
     <div className="container">
-      <ListView />
+      <ListView isloading={isloading} results={results} />
     </div>
   );
 }
--- src/components/ListView/index.jsx
+++ src/components/ListView/index.jsx
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react";
 import styles from './list.module.scss'
 import Loader from '../Loader'
 
-export default function ListView() {
-    const [results, setResults] = useState([]);
-    const [isloading, setLoading] = useState(false);
-    const [error, setError] = useState('');
-    useEffect(function(){
-        async function getCharaterDetails(){
-            try{
-                setLoading(true) 
-                setError('')
-                const res = await fetch(`https://rickandmortyapi.com/api/character`);
-                const data = await res.json();
-                setResults(data.results)
-                setLoading(false) 
-                console.log(data.results)
-            }catch(err){
-                console.error(`Error`, error)
-            }
-        }
-        getCharaterDetails()
-    },[])
+export default function ListView({
+    isloading,
+    results
+}) {
     return (
         <>
         {
