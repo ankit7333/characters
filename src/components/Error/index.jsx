7dfd1a22a44afdba43325029f260a2cf4afaf47e
ankitadulkar <ankit.adulkar1@justdial.com>
2024-01-26 18:44:30 +0530

    Added error component if api failed

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,3 @@
+export default function Loader(){
+    return <p className={`loader loader__error p-2`}>Error loading characters. Please try again later.</p>
+}
--- src/components/ListView/index.jsx
+++ src/components/ListView/index.jsx
@@ -1,5 +1,6 @@
 import styles from './list.module.scss'
 import Loader from '../Loader'
+import Error from '../Error'
 
 export default function ListView({
     isloading,
@@ -12,18 +13,22 @@ export default function ListView({
             isloading ? <Loader /> : (
                 <div className={`${styles.list}`}>
                     {
-                        results.map((value) => (
-                            <figure key={value.id}>
-                                <img src={value.image} alt={value.name} />
-                                <figcaption>
-                                    <div>Charater Name :- <b>{value.name}</b></div>
-                                    <div>Status :- <b className={`${value.status === 'Alive' ? 'color__green' : value.status === 'Dead' ? 'color__red' : 'color__grey'}`}>{value.status}</b></div>
-                                    <div>
-                                        <button className={`button button__primary`} onClick={() => onHandleDetail(value)}>More About {value.name}</button>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        ))
+                        results && results.length > 0 ? (
+                            results.map((value) => (
+                                <figure key={value.id}>
+                                    <img src={value.image} alt={value.name} />
+                                    <figcaption>
+                                        <div>Charater Name :- <b>{value.name}</b></div>
+                                        <div>Status :- <b className={`${value.status === 'Alive' ? 'color__green' : value.status === 'Dead' ? 'color__red' : 'color__grey'}`}>{value.status}</b></div>
+                                        <div>
+                                            <button className={`button button__primary`} onClick={() => onHandleDetail(value)}>More About {value.name}</button>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            ))
+                        ):(
+                            <Error />
+                        )
                     }
                 </div>
             )
